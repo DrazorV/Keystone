a08ff2efe626f6ca880f871ce8a38ce977f5cb03
Glitch (keystone-discordbot) <none>
2018-11-13 07:26:17 +0000

    💎🚔 Updated with Glitch

--- bot.js
+++ bot.js
@@ -4,7 +4,14 @@ const ytdl = require('ytdl-core');
 const YouTube = require('simple-youtube-api');
 const fs = require('fs');
 const queue = new Map();
-let guildArray = client.guilds.array();
+
+const http = require('http');
+const express = require('express'); 
+const app = express(); 
+app.get("/", (request, response) => response.sendStatus(200)); 
+app.listen(process.env.PORT);
+setInterval(() => {http.get('http://${process.env.PROJECT_DOMAIN}.glitch.me/'); }, 280000);
+
 
 client.on('warn', console.warn);
 
