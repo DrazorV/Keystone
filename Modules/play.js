857c098d2f2ec573665002e14fb7d8f0009a17ae
DrazorV <vagelisp.97@gmail.com>
2018-11-15 18:59:07 +0200

    Update play.js

--- Modules/play.js
+++ Modules/play.js
@@ -1,58 +1,46 @@
-const ytdl = require("ytdl-core");
-const join = require('./join');
 const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube('./env.goggle_api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube('AIzaSyCPY2W8byA60GchVn7fizxb6KaP1qjifZc');
 const queue = new Map();
+var ffmpeg = require('ffmpeg');
 
-var exports = module.exports = {
-    command: function (client,message) {
-        const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
+var  exports =  module.exports = {
+    async command (client, message) {
+        const args = message.content.slice(1).trim().split(/ +/g);
         const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
         const searchString = args.slice(1).join(' ');
-        var guild = client.guilds.get(message.guild.id);
         const voiceChannel = message.member.voiceChannel;
-        const permissions = voiceChannel.permissionsFor(message.client.user);
         if(!voiceChannel) {
             return message.channel.send("❌ You need to be in a voice channel to play music!");
         }
-
+        const permissions = voiceChannel.permissionsFor(message.client.user);
         if(!permissions.has('CONNECT')) {
             return message.channel.send("❌ I cannot connect to your voice channel, make sure I have the proper permissions!");
         }
         if(!permissions.has("SPEAK")){
             return message.channel.send('❌ I cannot speak in this voice channel, make sure I have the proper permissions!');
         }
-
-        if (!guild.voiceConnection) {
-            join.voiceJoin(message);
-        }
         if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-            const playlist = youtube.getPlaylist(url);
-            const videos = playlist.getVideos();
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
             for (const video of Object.values(videos)) {
-                const video2 = youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
-                exports.handleVideo(video2, message, voiceChannel, true); // eslint-disable-line no-await-in-loop
+                const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+                await exports.handleVideo(video2, message, voiceChannel, true); // eslint-disable-line no-await-in-loop
             }
             return message.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
         } else {
             try {
-                var video =  youtube.getVideo(url);
-            } catch (error) {
-                try {
-                    var videos = youtube.searchVideos(searchString, 10);
-                    var video = youtube.getVideoByID(videos[0].id);
-                }catch (err) {
-                    console.error(err);
-                    return message.channel.send('🆘 I could not obtain any search results.');
-                }
-                return exports.handleVideo(video, message, voiceChannel);
+                var videos = await youtube.searchVideos(searchString);
+                var video = await youtube.getVideoByID(videos[1].id);
+            }catch (err) {
+                console.error(err);
+                return message.channel.send('🆘 I could not obtain any search results.');
             }
+            return exports.handleVideo(video, message, voiceChannel);
         }
     },
-    handleVideo: function(video, message, voiceChannel, playlist = false) {
+    async handleVideo(video, message, voiceChannel, playlist = false) {
         const serverQueue = queue.get(message.guild.id);
-        console.log(video);
         const song = {
             id: video.id,
             title: video.title,
@@ -72,8 +60,9 @@ var exports = module.exports = {
             queueConstruct.songs.push(song);
 
             try {
-                queueConstruct.connection = voiceChannel.join();
-                exports.play(message.guild, queueConstruct.songs[0]);
+                queueConstruct.connection = await voiceChannel.join();
+                const song = queueConstruct.songs.pop();
+                exports.play(message.guild, song);
             } catch (error) {
                 console.error(`I could not join the voice channel: ${error}`);
                 queue.delete(message.guild.id);
@@ -87,7 +76,7 @@ var exports = module.exports = {
         }
         return undefined;
     },
-    play: function(guild,song) {
+    async play(guild,song){
         const serverQueue = queue.get(guild.id);
         if (!song) {
             serverQueue.voiceChannel.leave();
@@ -95,18 +84,21 @@ var exports = module.exports = {
             return;
         }
         console.log(serverQueue.songs);
+
         const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
             .on('end', reason => {
                 if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
                 else console.log(reason);
                 serverQueue.songs.shift();
-                exports.play(guild, serverQueue.songs[0]);
+                play(guild, serverQueue.songs[0]);
             })
             .on('error', error => console.error(error));
         dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
         serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
     }
 };
+
 
 
 
