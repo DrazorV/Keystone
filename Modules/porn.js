d33fe88957b2eb49fc99ac79c6a68acbac9356e9
DrazorV <vagelisp.97@gmail.com>
2018-12-15 21:13:27 +0200

    New commands on the way

--- Modules/porn.js
+++ Modules/porn.js
@@ -19,6 +19,7 @@ module.exports = {
                     embed.setColor(message.member.colorRole.color);
                     embed.setTimestamp(new Date());
                     embed.setFooter("Automated message", message.guild.iconURL);
+                    embed.setUrl(gif.webm);
                     message.channel.send(embed);
                 });
         }else{
