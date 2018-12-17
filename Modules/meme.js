3d61ac176f978e6a17692fcd5f7cfcd55c017db1
DrazorV <vagelisp.97@gmail.com>
2018-12-17 23:07:34 +0200

    New commands on the way

--- Modules/meme.js
+++ Modules/meme.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const embed = new Discord.RichEmbed();
 const meme = require('memejsfork');
+var schedule = require('node-schedule');
 
 
 
@@ -15,6 +16,19 @@ module.exports = {
             embed.setTimestamp(new Date());
             embed.setFooter("Automated message", message.guild.iconURL);
             message.channel.send(embed);
+        });
+
+        var j = schedule.scheduleJob('9 11 * * *', function () {
+            meme(function (data) {
+                embed.setURL("https://www.reddit.com/r/"+data.subreddit[0]);
+                embed.setDescription(data.title[0]);
+                embed.setImage(data.url[0]);
+                embed.setTitle("⚡Meme of the! 📦");
+                embed.setColor(message.member.colorRole.color);
+                embed.setTimestamp(new Date());
+                embed.setFooter("Automated message", message.guild.iconURL);
+                message.channel.send(embed);
+            })
         });
     }
 };
