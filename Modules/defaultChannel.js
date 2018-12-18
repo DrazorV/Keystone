8c2aef15a19c2fe6b2b51a4a1e3d0e28deeff3b0
DrazorV <vagelisp.97@gmail.com>
2018-12-18 21:29:52 +0200

    New commands on the way

--- Modules/defaultChannel.js
+++ Modules/defaultChannel.js
@@ -1,15 +1,18 @@
+const guilds = require("../guilds.json");
+
 module.exports = {
     command: function (channel,message) {
-        var channels = message.guild.channels.array();
+        const fs = require('fs');
+        const channels = message.guild.channels.array();
         while (channels.length > 0) {
-            var target = channels.pop();
+            const target = channels.pop();
             if ((channel === target.name) || (channel === target.id)) {
-                process.env.channel = target.id;
-                console.log(process.env.channel);
-                target.send("✅ This is the default meme's channel from now on.⚓");
+                guilds[message.guild.name] = target.id;
+                fs.writeFileSync("../guilds.json",JSON.stringify(guilds,null," "),"utf8");
+                target.send("This is the default channel for memes.");
                 return;
             }
         }
-        message.channel.send("❌This channel does not exist.\n Please try again!");
+        message.channel.send("This channel does not exist!");
     }
 };
