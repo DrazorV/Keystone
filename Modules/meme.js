c4582a9a4663af9bba66394846af865b87c7e05a
DrazorV <vagelisp.97@gmail.com>
2018-12-18 16:26:56 +0200

    New commands on the way

--- Modules/meme.js
+++ Modules/meme.js
@@ -1,32 +1,48 @@
 const Discord = require('discord.js');
 const embed = new Discord.RichEmbed();
 const meme = require('memejsfork');
+const isImageUrl = require('is-image-url');
 
 
-
-module.exports = {
+var modules = module.exports = {
     command: function (message) {
         meme(function(data) {
-            embed.setURL("https://www.reddit.com/r/"+data.subreddit[0]);
-            embed.setDescription(data.title[0]);
-            embed.setImage(data.url[0]);
-            embed.setTitle("Here is the meme you ordered! 📦");
-            embed.setColor(message.member.colorRole.color);
-            embed.setTimestamp(new Date());
-            embed.setFooter("Automated message", "https://cdn.discordapp.com/icons/308903005875470338/a306375be4d56f9dd85c5321f3f92343.jpg");
-            message.channel.send(embed);
+            if(isImageUrl(data.url[0])){
+                modules.createEmbed(message,data);
+                message.channel.send(embed);
+            }else{
+                modules.command(message)
+            }
         });
     },
     schedule: function (channel) {
         meme(function (data) {
-            embed.setURL("https://www.reddit.com/r/" + data.subreddit[0]);
-            embed.setDescription(data.title[0]);
-            embed.setImage(data.url[0]);
-            embed.setTitle("⚡Meme of the Day! 📦");
-            embed.setColor('#017E2D');
-            embed.setTimestamp(new Date());
-            embed.setFooter("Automated message", "https://cdn.discordapp.com/icons/308903005875470338/a306375be4d56f9dd85c5321f3f92343.jpg");
-            channel.send(embed);
+            if(isImageUrl(data.url[0])) {
+                modules.createEmbed2(channel, data);
+                channel.send(embed);
+            }else {
+                modules.schedule(channel);
+            }
         })
+    },
+    createEmbed:function (message,data) {
+        embed.setURL(data.url[0]);
+        embed.setTitle("Here is the meme you ordered! 📦");
+        embed.setDescription("🚛 Title: \n" + data.title[0]);
+        embed.addField("You can find more on the the subreddit: ", "https://www.reddit.com/r/" + data.subreddit[0], false);
+        embed.setImage(data.url[0]);
+        embed.setColor(message.member.colorRole.color);
+        embed.setTimestamp(new Date());
+        embed.setFooter("Automated message", "https://cdn.discordapp.com/icons/308903005875470338/a306375be4d56f9dd85c5321f3f92343.jpg");
+    },
+    createEmbed2:function (channel,data) {
+        embed.setURL(data.url[0]);
+        embed.setTitle("⚡Meme of the Day! 📦 ");
+        embed.setDescription("🚛 Title: " + data.title[0]);
+        embed.addField("You can find more on the the subreddit: ", "https://www.reddit.com/r/" + data.subreddit[0], false);
+        embed.setImage(data.url[0]);
+        embed.setColor('#017E2D');
+        embed.setTimestamp(new Date());
+        embed.setFooter("Automated message", "https://cdn.discordapp.com/icons/308903005875470338/a306375be4d56f9dd85c5321f3f92343.jpg");
     }
 };
