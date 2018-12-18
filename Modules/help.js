4f6115e5c6a05fdb5a3a6078ffe83a1af9cce4dc
DrazorV <vagelisp.97@gmail.com>
2018-12-18 23:12:44 +0200

    New commands on the way

--- Modules/help.js
+++ Modules/help.js
@@ -8,13 +8,13 @@ module.exports = {
     command: function (message,prefix) {
         embed.setTitle("List of Commands 📋");
         embed.setColor([255,90,0]);
-        embed.addField(prefix +"come @mention","🤙 Informs one or more server members that you are waiting for them in one of the voice channels",false);
         embed.addField(prefix + "avatar @mention","🎴 Gives you a link with his avatar",false);
+        embed.addField(prefix +"come @mention","🤙 Informs one or more server members that you are waiting for them in one of the voice channels",false);
+        embed.addField(prefix + "default [name/id]","🗓️ Sets the channel where the bot posts memes daily.");
+        embed.addField(prefix + "meme","🚧 To get a random meme from a huge collection",false);
         embed.addField(prefix + "ping","🏓 Starts a crazy ping-pong game",false);
         embed.addField(prefix + "prefix","🏷️ Lets you change the default prefix",false);
-        embed.addField(prefix + "meme","🚧 To get a random meme from a huge collection",false);
         embed.addField(prefix + "porn [input]","🔞 To get a random porn gif based on the input");
-        embed.addField(prefix + "default [name/id]","🗓️ Sets the channel where the bot posts memes daily.");
         embed.setTimestamp(new Date());
         embed.setFooter("Automated message", message.guild.iconURL);
         message.channel.send(embed);
