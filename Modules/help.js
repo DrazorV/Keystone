265fd8da6b9efd941ff0084a141c7d47e4d46b39
DrazorV <vagelisp.97@gmail.com>
2018-12-19 00:13:07 +0200

    New commands on the way

--- Modules/help.js
+++ Modules/help.js
@@ -9,7 +9,7 @@ module.exports = {
         embed.setTitle("List of Commands 📋");
         embed.setColor([255,90,0]);
         embed.addField(prefix + "avatar @mention","🎴 Gives you a link with his avatar",false);
-        embed.addField(prefix +"come @mention","🤙 Informs one or more server members that you are waiting for them in one of the voice channels",false);
+        embed.addField(prefix + "come @mention","🤙 Informs one or more server members that you are waiting for them in one of the voice channels",false);
         embed.addField(prefix + "default [name/id]","🗓️ Sets the channel where the bot posts memes daily.");
         embed.addField(prefix + "meme","🚧 To get a random meme from a huge collection",false);
         embed.addField(prefix + "ping","🏓 Starts a crazy ping-pong game",false);
