77fc2eac7d14a021c0b9f274dfa1a7d10c422cbe
DrazorV <vagelisp.97@gmail.com>
2018-12-20 16:57:28 +0200

    Optimized code!

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const config = require('./config');
 const Enmap = require("enmap");
 client.commands = new Enmap();
 
