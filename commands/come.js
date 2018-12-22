02ce5b2132f9b5d5f1c258d5d16fe2b1f4533ecb
DrazorV <vagelisp.97@gmail.com>
2018-12-22 18:50:03 +0200

    Optimized code!

--- commands/come.js
+++ commands/come.js
@@ -14,31 +14,24 @@ exports.run = async (client,message,args)=>{
     embed.setTimestamp(new Date());
     embed.setFooter("Automated message", message.guild.iconURL);
     let targets = message.mentions.users.array();
-    const channel = message.member.voiceChannel;
-    if (channel == null) {
-        while (targets.length > 0) {
-            const user = targets.pop();
-            if (user.bot) message.channel.send("❌ Bots usually don't hang out with humans!");
-            else {
+    while (targets.length > 0) {
+        const user = targets.pop();
+        if (user.bot) message.channel.send("❌ Bots usually don't hang out with humans!");
+        else {
+            const channel = message.member.voiceChannel;
+            if (channel == null) {
                 embed.setDescription("You can choose one of the voice channels and he will join you ASAP");
                 user.send(embed);
                 message.channel.send("✅ " + user.username + " has been informed!");
-            }
-        }
-    } else {
-        embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
-        while (targets.length > 0) {
-            const user = targets.pop();
-            if (user.bot) message.channel.send("❌ Bots usually don't hang out with humans!");
-            else {
+            } else {
+                embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
                 if (!channel.members.has(user.id)) {
                     message.channel.send("✅ " + user.username + " has been informed!");
                     user.send(embed)
                         .then(message.member.voiceChannel.createInvite(options)
                             .then(invite => user.send(invite.toString()))
                             .catch(console.error));
-                }
-                else message.channel.send("❌ " + user.username + " is already in your voice channel!");
+                } else message.channel.send("❌ " + user.username + " is already in your voice channel!");
             }
         }
     }
