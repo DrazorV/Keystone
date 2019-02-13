0e5a02d54f37001cc9e7fe4538fb297fb0db6375
DrazorV <vagelisp.97@gmail.com>
2019-02-13 19:50:55 +0200

    New functions for the /come command

--- commands/come.js
+++ commands/come.js
@@ -14,7 +14,7 @@ exports.run = async (client,message,args)=>{
     embed.setTimestamp(new Date());
     embed.setFooter("Automated message", message.guild.iconURL);
     let roles = [];
-    let bool = false;
+    let bool = false,bool2 = false;
     for(const rol of message.mentions.roles.array()) for(const mem of rol.members.array()) roles.push(mem.user);
     let targets = message.mentions.users.array();
     for (const rol of roles) if (!targets.includes(rol)) targets.push(rol);
@@ -28,11 +28,13 @@ exports.run = async (client,message,args)=>{
                 if (user !== message.author) {
                     user.send(embed);
                     message.channel.send("✅ " + user.username + " has been informed!");
+                    bool2 = true;
                 }
             } else {
                 embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
                 if (!channel.members.has(user.id)) {
                     message.channel.send("✅ " + user.username + " has been informed!");
+                    bool2 = true;
                     user.send(embed)
                         .then(message.member.voiceChannel.createInvite(options)
                             .then(invite => user.send(invite.toString()))
@@ -41,5 +43,5 @@ exports.run = async (client,message,args)=>{
             }
         }
     }
-    if(bool) message.channel.send("❌ Offline users and bots cannot be invited.");
+    if(bool&&!bool2) message.channel.send("❌ Offline users and bots cannot be invited.");
 };
