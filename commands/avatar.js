46ef05fbfd721e19fe1da537f2fc68924caf5ee0
DrazorV <vagelisp.97@gmail.com>
2019-03-02 14:56:33 +0200

    New functions for the /come command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,19 +1,22 @@
-
-
+const Discord = require('discord.js');
 
 exports.run = async (client,message,args)=>{
     let targets = message.mentions.users.array();
     try{
         message.delete();
     }catch (e) {
-
     }
     while (targets.length>0){
         const user = targets.pop();
-        try{
-            message.channel.send(user.username + "'s avatar",{
-                file: "https://cdn.discordapp.com/avatars/"+ user.id + "/" + user.avatar + ".png"
-            });
+        try {
+            const embed = new Discord.RichEmbed();
+            embed.setURL(user.avatarURL);
+            embed.setTitle("🖼️ Here is " + user.username + "'s Avatar");
+            embed.setImage(user.avatarURL);
+            embed.setColor(message.member.colorRole.color);
+            embed.setTimestamp(new Date());
+            embed.setFooter("Automated message", "https://cdn.discordapp.com/icons/308903005875470338/a306375be4d56f9dd85c5321f3f92343.jpg");
+            message.channel.send(embed);
         }catch (e) {
             message.channel.send("This user has no Avatar.")
         }
