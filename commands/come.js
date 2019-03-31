e6ef956214d70849cc2c721a1208a766ebe7927e
DrazorV <vagelisp.97@gmail.com>
2019-03-31 16:18:25 +0300

    New functions for the /come command

--- commands/come.js
+++ commands/come.js
@@ -14,16 +14,13 @@ exports.run = async (client,message,args)=>{
     embed.setTimestamp(new Date());
     embed.setFooter("Automated message", message.guild.iconURL);
     let roles = [];
-    let bool = false,bool2 = false,bool3 = false;
+    let bool = false,bool2 = true,bool3 = false;
     for(const rol of message.mentions.roles.array()) for(const mem of rol.members.array()) roles.push(mem.user);
     let targets = message.mentions.users.array();
+    if(targets.length > 0) bool3 = true;
     for (const rol of roles) if (!targets.includes(rol)) targets.push(rol);
     if(targets.length === 1) bool3 = true;
-    try{
-        message.delete();
-    }catch (e) {
 
-    }
     while (targets.length > 0) {
         const user = targets.pop();
         if ((user.bot||user.presence.status !== "online")&& !bool3) bool = true;
@@ -34,13 +31,13 @@ exports.run = async (client,message,args)=>{
                 if (user !== message.author) {
                     user.send(embed);
                     message.channel.send("✅ " + user.username + " has been informed!");
-                    bool2 = true;
+                    bool2 = false;
                 }
             } else {
                 embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
                 if (!channel.members.has(user.id)) {
                     message.channel.send("✅ " + user.username + " has been informed!");
-                    bool2 = true;
+                    bool2 = false;
                     user.send(embed)
                         .then(message.member.voiceChannel.createInvite(options)
                             .then(invite => user.send(invite.toString()))
@@ -49,5 +46,5 @@ exports.run = async (client,message,args)=>{
             }
         }
     }
-    if(bool&&!bool2) message.channel.send("❌ Offline users and bots cannot be invited.");
+    if(bool && bool2) message.channel.send("❌ Offline users and bots cannot be invited.");
 };
