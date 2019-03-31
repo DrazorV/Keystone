b8c08ce7b60c890d115b2d3606238c3af845042c
DrazorV <vagelisp.97@gmail.com>
2019-03-31 17:04:38 +0300

    New functions for the /come command

--- commands/come.js
+++ commands/come.js
@@ -14,37 +14,55 @@ exports.run = async (client,message,args)=>{
     embed.setTimestamp(new Date());
     embed.setFooter("Automated message", message.guild.iconURL);
     let roles = [];
-    let bool = false,bool2 = true,bool3 = false;
+    let bool = false, bool2 = true;
     for(const rol of message.mentions.roles.array()) for(const mem of rol.members.array()) roles.push(mem.user);
     let targets = message.mentions.users.array();
-    if(targets.length > 0) bool3 = true;
-    for (const rol of roles) if (!targets.includes(rol)) targets.push(rol);
-    if(targets.length === 1) bool3 = true;
+    let targets2 = [];
+    for (const rol of roles) if (!targets.includes(rol)) targets2.push(rol);
 
     while (targets.length > 0) {
         const user = targets.pop();
-        if ((user.bot||user.presence.status !== "online")&& !bool3) bool = true;
+        if (user.bot||user.presence.status === "offline") bool = true;
         else {
-            const channel = message.member.voiceChannel;
-            if (channel == null) {
-                embed.setDescription("You can choose one of the voice channels and he will join you ASAP");
-                if (user !== message.author) {
-                    user.send(embed);
-                    message.channel.send("✅ " + user.username + " has been informed!");
-                    bool2 = false;
+            createEmbed(message,embed,user)
+        }
+    }
+
+    if(targets2 !== null) {
+        while (targets2.length > 0) {
+            const user = targets2.pop();
+            if (user.bot || user.presence.status !== "online") bool = true;
+            else {
+                const channel = message.member.voiceChannel;
+                if (channel == null) {
+                    if (user !== message.author) bool2 = false;
+                } else {
+                    if (!channel.members.has(user.id)) bool2 = false;
                 }
-            } else {
-                embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
-                if (!channel.members.has(user.id)) {
-                    message.channel.send("✅ " + user.username + " has been informed!");
-                    bool2 = false;
-                    user.send(embed)
-                        .then(message.member.voiceChannel.createInvite(options)
-                            .then(invite => user.send(invite.toString()))
-                            .catch(console.error));
-                } else if (user !== message.author) message.channel.send("❌ " + user.username + " is already in your voice channel!");
+                createEmbed(message, embed, user)
             }
         }
     }
-    if(bool && bool2) message.channel.send("❌ Offline users and bots cannot be invited.");
+    if(bool && bool2) message.channel.send("❌ Inactive users and bots cannot be invited.");
 };
+
+
+function createEmbed(message, embed,user){
+    const channel = message.member.voiceChannel;
+    if (channel == null) {
+        embed.setDescription("You can choose one of the voice channels and he will join you ASAP");
+        if (user !== message.author) {
+            user.send(embed);
+            message.channel.send("✅ " + user.username + " has been informed!");
+        }
+    } else {
+        embed.setDescription(":arrow_down: Click the button bellow to join him :arrow_down:");
+        if (!channel.members.has(user.id)) {
+            message.channel.send("✅ " + user.username + " has been informed!");
+            user.send(embed)
+                .then(message.member.voiceChannel.createInvite(options)
+                    .then(invite => user.send(invite.toString()))
+                    .catch(console.error));
+        } else if (user !== message.author) message.channel.send("❌ " + user.username + " is already in your voice channel!");
+    }
+}
