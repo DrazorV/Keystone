860965f2af603d9d1bb939097b777a62c48ed218
DrazorV <vagelisp.97@gmail.com>
2020-07-24 00:04:01 +0300

    improved prefix command interaction

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,17 +1,14 @@
 const prefixs = require('../data/prefixs.json');
 const fs = require('fs');
-const Discord = require('discord.js');
 
 exports.run = async (client,message,args)=>{
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You need to be an admin to change the prefix of the bot.");
     await message.channel.send("💻 Please type the new prefix");
-    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {maxMatches: 1, time: 15000 });
-    collector.on("collect",prefix => {
-        prefixs[message.guild.id] = prefix.content;
+    message.channel.awaitMessages(m => m.author.id === message.author.id, {max: 1, time: 30000}).then(collected => {
+        prefixs[message.guild.id] = collected.first().content;
         fs.writeFileSync('./data/prefixs.json', JSON.stringify(prefixs, null, "\t"), "utf8");
-    });
-    collector.on("end",received => {
-        if (received.array().length===0) message.channel.send("I've got tired of waiting! 😫 \nPlease try again! 🔁");
-        else message.channel.send("🎉 The 🆕 prefix is set to \"" + prefixs[message.guild.id] + "\"");
-    });
+        message.channel.send("🎉 The 🆕 prefix is set to \"" + prefixs[message.guild.id] + "\"");
+    }).catch(()=>{
+        message.channel.send("I've got tired of waiting! 😫 \nPlease try again! 🔁");
+    })
 };
