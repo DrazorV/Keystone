7235a2dda2d98cf60984b15ac72bdeab9915c906
DrazorV <vagelisp.97@gmail.com>
2020-07-30 23:27:15 +0300

    Commit

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name: 'Keystone',
+    script: 'bot.js',
+    watch: 'true',
+    ignore_watch: ['json.sqlite','node_modules'],
+    watch_options: {
+      "followSymlinks": false
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : '192.168.1.200',
+      ref  : 'origin/master',
+      repo : 'git@github.com:DrazorV/Keystone.git',
+      path : 'home/pi/Repos/Keystone',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
