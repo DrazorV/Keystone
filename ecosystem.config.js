f7a6353b00a1595a10d314dc98c5ebd3047e59e3
DrazorV <vagelisp.97@gmail.com>
2020-07-31 14:04:12 +0300

    Testing Docker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,12 +15,12 @@ module.exports = {
     ],
     deploy: {
         production: {
-            user: 'rapsi',
+            user: 'raspi',
             host: '192.168.1.200',
             ref: 'origin/master',
             repo: 'git@github.com:DrazorV/Keystone.git',
             path: '/home/pi/Repos/Keystone',
-            'post-deploy': 'npm install; npm run build; pm2 startOrRestart ecosystem.config.js --env production',
+            'post-deploy': 'npm install; npm run build; pm2 startOrRestart ecosystem.config.js --env production && pm2 save',
         },
     },
 };
