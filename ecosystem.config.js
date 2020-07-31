361dec6e35600b97d2ae092d5c7c7d0901728ac1
DrazorV <vagelisp.97@gmail.com>
2020-07-31 10:58:52 +0300

    Ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,21 +3,20 @@ module.exports = {
     {
       name: 'Keystone',
       script: 'bot.js',
-      env_production: {
-        NODE_ENV: "production"
-      }
     }
   ],
   deploy : {
     production : {
+      key  : '~/.ssh/id_rsa',
       user : 'pi',
       host : '192.168.1.200',
       ref  : 'origin/master',
       repo : 'git@github.com:DrazorV/Keystone.git',
       path : '',
-      'pre-deploy-local': '',
-      'post-deploy' : "export PATH=$PATH:/home/pi/.nvm/v12.18.3/bin/" && 'npm install && pm2 reload ecosystem.config.js --env production && pm2 save',
-      'pre-setup': ''
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production && pm2 save',
+      'post-setup': "ls -la",
+      'pre-setup': 'apt-get install git ; ls -la'
     }
   }
 };
