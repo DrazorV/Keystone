964a37eccf441877826381b31a9673d209e9eab8
Vagelis Panos <vagelisp.97@gmail.com>
2021-01-06 22:52:11 +0200

    Setting up github webhooks

--- payload.js
+++ payload.js
@@ -2,6 +2,7 @@ const winston = require('./utils/winston');
 
 let express = require('express');
 let bodyParser = require('body-parser');
+const res = require("express");
 let app = express();
 let exec = require('child_process').exec;
 
@@ -13,14 +14,17 @@ app.post('/payload', function (req, res) {
     //verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
     winston.info(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
 
-    winston.info('pulling code from GitHub...');
-
     winston.warn('pulling code from GitHub...');
     exec('git -C ~/Repos/Keystone reset --hard', execCallback);
     exec('git -C ~/Repos/Keystone  clean -df', execCallback);
     exec('git -C ~/Repos/Keystone  pull -f', execCallback);
     exec('npm -C ~/Repos/Keystone  install --production', execCallback);
     exec('tsc', execCallback);
+});
+
+app.get('/payload', function (req, res) {
+    res.sendStatus(200);
+    console.log('get /payload');
 });
 
 app.listen(9000, function () {
